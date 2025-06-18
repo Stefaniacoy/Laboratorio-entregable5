de199ea6a057258b0e6d4c6bb7421e2663eaaed0
Stefania Coy <stefaniacoy97@gmail.com>
2025-06-17 22:48:01 -0500

    Desarrollo del paso 1

---
+++ paso1.py
@@ -0,0 +1,15 @@
+print("¡Hola! Bienvenido al mundo del aprendizaje")
+
+nombre = input("¿Cómo te llamas?")
+
+edad = int(input("¿Cuántos años tienes?"))
+
+numero1 = float(input("Escribe el primer numero que quieres sumar:"))
+numero2 = float(input("Escribe el segundo numero que quieres sumar:"))
+
+resultado = numero1 + numero2
+
+print("Hola" + nombre + ", tienes", edad, "años.")
+print("La suma de", numero1, "+", numero2, "es:", resultado)
+
+print("¡Hasta pronto! " + nombre + ", gracias por usar este programa")
