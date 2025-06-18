597d124989885fc8b2db0392d6c9a0b6477fa56b
Stefania Coy <stefaniacoy97@gmail.com>
2025-06-18 16:42:38 -0500

    Paso 4 juego de adivinanzas

---
+++ paso4-juego.py
@@ -0,0 +1,20 @@
+import random
+
+def juego_adivinanza():
+    numero_secreto = random.randint(1, 10)
+    intentos = 0
+    print("¡Adivina el numero entre 1 y 10!")
+    
+    while True: 
+        intento = int(input("Ingresa tu número:"))
+        intentos += 1
+        
+        if intento < numero_secreto:
+            print("El número es mayor.")
+        elif intento > numero_secreto:
+            print("El número es menor.")
+        else: 
+            print(f"¡Felicidades! Adivinaste el número en {intentos} intentos.")
+            break
+        
+juego_adivinanza()
